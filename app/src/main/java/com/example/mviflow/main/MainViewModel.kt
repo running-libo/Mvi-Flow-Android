c4b18e671cf2aa76be2a43739cebf27185c3ff41
libo <libo31@xiaomi.com>
2023-11-06 00:03:37 +0800

    对success  code判断

--- app/src/main/java/com/example/mviflow/main/MainViewModel.kt
+++ app/src/main/java/com/example/mviflow/main/MainViewModel.kt
@@ -2,10 +2,9 @@ package com.example.mviflow.main
 
 import com.example.mviflow.base.BaseViewModel
 import com.example.mviflow.base.IUiIntent
-import com.example.mviflow.model.http.WanRetrofitClient
-import com.example.mviflow.model.http.service.WanApi
 
 class MainViewModel : BaseViewModel<MainState, MainIntent>() {
+    private val repos = MainRepository()
 
     override fun handleIntent(intent: IUiIntent) {
         //viewModel处理Intent，判断intent类型来做对应的事情
@@ -14,7 +13,7 @@ class MainViewModel : BaseViewModel<MainState, MainIntent>() {
                 requestDataWithFlow(
                     showLoading = true,
                     request = {
-                        WanRetrofitClient.getService(WanApi::class.java).getArticle(0)
+                        repos.requestRankData(0)
                     },
                     successCallback = { data ->
                         //请求成功进行UI状态回调
